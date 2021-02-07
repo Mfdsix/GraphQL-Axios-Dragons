7795a4f2a98644839e5910e19869966dc25794c4
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-02-08 01:06:39 +0700

    add cors for api

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const {
 } = require("express-graphql")
 const dragonSchema = require("./schema/dragon")
 const app = express()
+const cors = require("cors")
 
+app.use(cors)
 app.use("/dragons", graphqlHTTP({
     schema: dragonSchema,
     graphiql: true,
